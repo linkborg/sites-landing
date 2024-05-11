05f672e62b808795179c0f673a8f95bfbd1c884f
shubhayu-64 <majumdarshubhayu@gmail.com>
2024-05-11 10:08:46 +0530

    ui fixes

--- src/index.tsx
+++ src/index.tsx
@@ -29,11 +29,13 @@ name,
 description,
 }) => (
 <div className="text-center mb-8 -mt-16">
-    <img
-    src={logo}
-    alt="Avatar"
-    className="rounded-full bg-blue-500 w-32 h-32 mx-auto mb-4"
-    />
+    <div className="w-36 h-36 mx-auto flex items-center justify-center">
+        <img
+            src={logo}
+            alt="Avatar"
+            className="rounded-full border-2 border-black p-1 w-32 h-32 bg-black"
+        />
+    </div>
     <h1 className="text-2xl font-bold mt-4">{name}</h1>
     <p className="text-gray-400">{description}</p>
 </div>
